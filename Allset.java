27332f08cf7d981493b476d4e9d1f2d21072ca50
ddeshya <uddeshyasingh1111@gmail.com>
2023-08-29 08:49:02 -0700

    CC

---
+++ Allset.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.*;
+
+
+class Allset{
+    public static void main (String [] args ){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Name Please ");
+        double a= input.nextDouble();
+        System.out.println(a);
+        System.out.println("roll");
+        int n = input.nextInt();
+        System.out.println(n);
+    }
+}
